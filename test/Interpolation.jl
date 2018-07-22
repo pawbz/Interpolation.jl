f4703e0d470563ea9dba1656446931305b8782d2
Pawan Bharadwaj <bharadwaj.pawan@gmail.com>
2018-07-22 09:11:54 -0400

    Major update: API to use LinearMaps

--- test/Interpolation.jl
+++ test/Interpolation.jl
@@ -7,56 +7,59 @@ nz=Array(linspace(1,20,30));
 mz=Array(linspace(5,27,40));
 
 
+for Battrib in [:B1, :B2]
+
+	pa=Interpolation.Kernel([nx], [mx], Battrib)
+	println("=====================================")
+	## 1D
+	ny=randn(length(nx));
+	# interpolate (my=f(ny))
+	my=zeros(length(mx));
+	@btime Interpolation.interp_spray!(ny, my, pa, :interp)
+
+	myp = randn(length(my));
+
+	# spray (nyp=fˣ(myp))
+	nyp=zeros(length(nx));
+	@btime Interpolation.interp_spray!(nyp, myp, pa, :spray)
+
+	# dot product test
+	@test dot(my, myp) ≈ dot(ny, nyp)
+end
+
+
+"""
+Interpolation on the same grid should not change for B1
+"""
 Battrib=:B1
-
-pa=Interpolation.Param([nx], [mx], Battrib)
-println("=====================================")
-## 1D
-ny=randn(length(nx));
-# interpolate (my=f(ny))
-my=zeros(length(mx));
-@btime Interpolation.interp_spray!(ny, my, pa, :interp)
-
-myp = randn(length(my));
-
-# spray (nyp=fˣ(myp))
-nyp=zeros(length(nx));
-@btime Interpolation.interp_spray!(nyp, myp, pa, :spray)
-
-# dot product test
-@test dot(my, myp) ≈ dot(ny, nyp)
-
-"""
-Interpolation on the same grid should not change
-"""
-pa=Interpolation.Param([nx, nz], [nx, nz], Battrib)
+pa=Interpolation.Kernel([nx, nz], [nx, nz], Battrib)
 println("=====================================")
 ny=randn(length(nz), length(nx));
 my=zeros(length(nz), length(nx));
 @btime Interpolation.interp_spray!(ny, my, pa, :interp)
 @test ny≈my
+
 myp = randn(size(my));
 nyp=zeros(length(nz), length(nx));
 @btime Interpolation.interp_spray!(nyp, myp, pa, :spray)
 @test dot(my, myp) ≈ dot(ny, nyp)
 
-pa=Interpolation.Param([nx, nz], [mx, mz], Battrib)
-println("=====================================")
 
-## 2D
-ny=randn(length(nz), length(nx));
-# interpolate
-my=zeros(length(mz), length(mx));
-@btime Interpolation.interp_spray!(ny, my, pa, :interp)
-
-
-myp = randn(size(my));
-
-# spray
-nyp=zeros(length(nz), length(nx));
-@btime Interpolation.interp_spray!(nyp, myp, pa, :spray)
-
-# dot product test
-@test dot(my, myp) ≈ dot(ny, nyp)
+for Battrib in [:B1, :B2]
+	pa=Interpolation.Kernel([nx, nz], [mx, mz], Battrib)
+	## 2D
+	ny=randn(length(nz), length(nx));
+	# interpolate
+	my=zeros(length(mz), length(mx));
+	@btime Interpolation.interp_spray!(ny, my, pa, :interp)
 
 
+	myp = randn(size(my));
+
+	# spray
+	nyp=zeros(length(nz), length(nx));
+	@btime Interpolation.interp_spray!(nyp, myp, pa, :spray)
+
+	# dot product test
+	@test dot(my, myp) ≈ dot(ny, nyp)
+end
