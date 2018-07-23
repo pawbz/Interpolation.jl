e1a33a215701bee208d97229db6503b7e69cd640
Pawan Bharadwaj <bharadwaj.pawan@gmail.com>
2018-07-22 22:42:31 -0400

    F1 and F2 for 2D interp

--- src/Interpolation.jl
+++ src/Interpolation.jl
@@ -14,9 +14,9 @@ using LinearMaps
 abstract type Kernel end
 
 mutable struct Kernel_2D{T<:Real} <: Kernel
-	F::SparseMatrixCSC{T,Int64}
-	x::Vector{T} # just allocating input
-	y::Vector{T} # just allocating output
+	F1::SparseMatrixCSC{T,Int64}
+	F2::SparseMatrixCSC{T,Int64}
+	xx::Matrix{Float64}
 end
 
 mutable struct Kernel_1D{T<:Real} <: Kernel
@@ -28,30 +28,32 @@ function Kernel(x::Array{Vector{Float64}},
 		  xi::Array{Vector{Float64}}, Battrib::Symbol=:B1)
 
 
-	pa=P_core(x,xi,Battrib)
 
-	xin=zeros(flipdim(pa.nx,1)...)
-	xout=zeros(flipdim(pa.nxi,1)...)
 	# constructor for y=Ax
-	function interp!(y, x)
+	function interp!(y, x, pa)
 		for i in eachindex(y)
 			y[i]=0.0
 		end
-		copy!(xin,x)
-		Interpolation.interp_spray!(xin, xout, pa, :interp)
-		copy!(y,xout)
+		Interpolation.interp_spray!(x, y, pa, :interp)
 	end
 
-	F=LinearMap(interp!, nothing, prod(pa.nxi), prod(pa.nx), ismutating=true)
-
-	F=SparseArrays.sparse(F)
-
-	if(length(pa.nx)==1)
-		return Kernel_1D(F)
-	elseif(length(pa.nx)==2)
-		x=zeros(prod(pa.nx))
-		y=zeros(prod(pa.nxi))
-		return Kernel_2D(F,x,y)
+	if(length(x)==1)
+		pa=P_core(x,xi,Battrib)
+		F1=LinearMap((y,x)->interp!(y,x,pa), 
+		      nothing, length(xi[1]), length(x[1]), ismutating=true)
+		F1=SparseArrays.sparse(F1)
+		return Kernel_1D(F1)
+	elseif(length(x)==2)
+		pa1=P_core(x[2:2],xi[2:2],Battrib)
+		F1=LinearMap((y,x)->interp!(y,x,pa1), 
+		      nothing, length(xi[2]), length(x[2]), ismutating=true)
+		F1=SparseArrays.sparse(F1)
+		pa2=P_core(x[1:1],xi[1:1],Battrib)
+		F2=LinearMap((y,x)->interp!(y,x,pa2), 
+		      nothing, length(xi[1]), length(x[1]), ismutating=true)
+		F2=SparseArrays.sparse(F2)
+		xx=zeros(size(F1,1), length(x[1]))
+		return Kernel_2D(F1,F2,xx)
 	else
 		error("dimension should be <=2")
 
@@ -68,13 +70,11 @@ function interp_spray!(y, yi, pa::Kernel_1D, attrib)
 end
 function interp_spray!(y, yi, pa::Kernel_2D, attrib)
 	if(attrib == :interp)
-		copy!(pa.x,y)
-		A_mul_B!(pa.y, pa.F, pa.x)
-		copy!(yi,pa.y)
+		A_mul_B!(pa.xx, pa.F1, y)
+		A_mul_Bc!(yi, pa.xx, pa.F2)
 	elseif(attrib == :spray)
-		copy!(pa.y,yi)
-		Ac_mul_B!(pa.x, pa.F, pa.y)
-		copy!(y,pa.x)
+		A_mul_B!(pa.xx, yi, pa.F2)
+		Ac_mul_B!(y, pa.F1, pa.xx)
 	end
 end
 
